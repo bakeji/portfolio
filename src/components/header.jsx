2b7bfd1333b79df3b2f3f3ec6ddf6bd379a274c2
bakeji <fatimohbakare3@gmail.com>
2023-09-26 18:50:55 +0100

    added media query for mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,11 +7,14 @@ return(
         <h1>Fatimoh Bakare</h1>
     </div>
 
+        <button className="ham"><img src="/menu.png" alt="" /></button>
+
     <nav>
         <a href="#app">Home</a>
         <a href="#About">About</a>
         <a href="#Projects">Project</a>
         <a href="#Contact">Contact</a>
+
     </nav>
    </header>
 )
