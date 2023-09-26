8453c7790f75cc22b1686e580d137b6d002e470b
bakeji <fatimohbakare3@gmail.com>
2023-09-26 19:23:43 +0100

    added media query for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/header";
 import About from "./components/about";
 import Projects from "./components/projects";
@@ -6,9 +6,16 @@ import Contact from "./components/contact";
 import Footer from "./components/footer";
 
 export default function App(){
+  const [showNav, setShowNav] = useState(false)
+
+  const toggleNav=()=>{
+    setShowNav(prevNav => !prevNav)
+  }
   return(
     <div id="app">
-      <Header />
+      <Header 
+      toggleNav = {toggleNav} 
+      showNav = {showNav}/>
       <About />
       <Projects />
       <Contact />
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-export default function Header(){
+export default function Header(props){
 return(
    <header>
     <div className="logo">
         <h1>Fatimoh Bakare</h1>
     </div>
 
-        <button className="ham"><img src="/menu.png" alt="" /></button>
+        <button onClick={props.toggleNav} className="ham"><img src={props.showNav? "/cross.png": "/hamburger.png"} alt="" /></button>
 
-    <nav>
+    <nav className={props.showNav? "show" : "hide"}>
         <a href="#app">Home</a>
         <a href="#About">About</a>
         <a href="#Projects">Project</a>
